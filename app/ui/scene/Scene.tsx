01ea0f3a58bd0f827ae2c821d65f8af12e943ea4
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-11-11 19:41:19 +0000

    fix: change scene

--- app/ui/scene/Scene.tsx
+++ app/ui/scene/Scene.tsx
@@ -1,125 +1,62 @@
+import Image from "next/image";
 import { units } from "@/app/lib/data";
 import styles from "./Scene.module.css";
-import Image from "next/image";
 
 export default function Scene() {
   return (
     <div className={styles.scene}>
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.orphan.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.betrayal.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.loneliness.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.place.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.loneliness.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.violence.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.betrayal.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.orphan.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
 
       <div className={styles.item}>
-        <Image
-          src={"/images/house.png"}
-          width={100}
-          height={100}
-          alt={""}
-          className={styles.image}
-        />
-        <div className={styles.unit}>
+        <button className={`${styles.unit} ${styles.active}`}>
           <Image src={units.loneliness.src} width={70} height={70} alt={""} />
-        </div>
+        </button>
       </div>
     </div>
   );
