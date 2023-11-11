c4e9405f046e77b71fb216bbebaab0da69c05dd2
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-11-11 22:21:48 +0100

    feat: leaderboard added

--- api/settings.py
+++ api/settings.py
@@ -5,11 +5,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_ROOT_USER = os.getenv('DB_ROOT_USER')
-DB_ROOT_PASSWORD = os.getenv('DB_ROOT_PASSWORD')
 ALGORITHM = ['HS256']
 
-SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./hack_game3_db'
