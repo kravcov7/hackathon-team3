4a4d070f0e1e06f43e93a8dea12903f5e2da9455
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-11-11 16:26:28 +0000

    refactor: add units image

---
+++ app/lib/data.ts
@@ -0,0 +1,17 @@
+export const units = {
+  betrayal: {
+    src: "/images/fears/betrayal.png",
+  },
+  loneliness: {
+    src: "/images/fears/loneliness.png",
+  },
+  orphan: {
+    src: "/images/fears/orphan.png",
+  },
+  place: {
+    src: "/images/fears/place.png",
+  },
+  violence: {
+    src: "/images/fears/violence.png",
+  },
+};
