d37d66bd848855f16e536840c0d2151c6ec08da2
Artem K <a.kravtsov@mish.design>
2023-11-11 14:21:00 +0300

    feat/edit main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-   <main className={styles.main}>
+    <main className={styles.main}>
       <h1 className={styles.title}>Shoot Children's Scares</h1>
       <p className={styles.subtitle}>
         Point-and-click-игрушка, где надо "отстреливать" страхи детей.
@@ -12,24 +12,29 @@ export default function Home() {
       <div className={styles.buttons}>
         <Link
           href="/second"
-          className={styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"}
+          className={
+            styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"
+          }
         >
           Играть
         </Link>
         <Link
           href="/form/signup"
-          className={styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"}
+          className={
+            styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"
+          }
         >
           Регистрация
         </Link>
 
         <Link
           href="/form"
-          className={styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"}
+          className={
+            styles.button + " py-2 px-5 rounded-lg bg-indigo-600 text-white"
+          }
         >
           Войти в аккаунт
         </Link>
-        
       </div>
     </main>
   );
