ed6d39d9f5f48e46fc55bd039211492d39877b9d
Elvira Krutova <77353395+Elvira-del@users.noreply.github.com>
2023-11-12 12:07:42 +0000

    feat: add legends of fears

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -26,4 +26,34 @@ export const units = [
     src: "/images/fears/violence.png",
     desc: "A hand stopping a fist: Indicates the fear of violence.",
   },
+  {
+    id: uuidv4(),
+    src: "/images/fears/dark.png",
+    desc: "A man in a dark room: Means the fear of darkness.",
+  },
+  {
+    id: uuidv4(),
+    src: "/images/fears/doctor.png",
+    desc: "A doctor: Represents the fear of doctors and the formal procedures associated with orphanages.",
+  },
+  {
+    id: uuidv4(),
+    src: "/images/fears/mask.png",
+    desc: "Two-faced person: An example of the fear of betrayal and lies.",
+  },
+  {
+    id: uuidv4(),
+    src: "/images/fears/sad.png",
+    desc: "A girl in rainy weather: Means the fear of loneliness and not having a home.",
+  },
+  {
+    id: uuidv4(),
+    src: "/images/fears/unknown.png",
+    desc: "A green face with a question mark: Illustrates the fear of unknown people or families.",
+  },
+  {
+    id: uuidv4(),
+    src: "/images/fears/orphan-1.png",
+    desc: "Broken heart and a house: Symbolizes the fear of losing a home and family.",
+  },
 ];
