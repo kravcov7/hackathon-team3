dc42d3930f9d850227d5b676e9e040cfabc2f921
Artem K <a.kravtsov@mish.design>
2023-11-12 16:57:16 +0300

    feature/edit text

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -4,56 +4,56 @@ export const units = [
   {
     id: uuidv4(),
     src: "/images/fears/betrayal.png",
-    desc: "Represents the fear being betrayed by loved ones.",
+    desc: "Cтрах предательства со стороны близких",
   },
   {
     id: uuidv4(),
     src: "/images/fears/loneliness.png",
-    desc: "A boy sitting with his head down: Also represents the fear of loneliness.",
+    desc: "Страх одиночества",
   },
   {
     id: uuidv4(),
     src: "/images/fears/orphan.png",
-    desc: "A little boy and a big gray person in the background: Shows the fear of being left by one's family.",
+    desc: "Страх быть оставленным своей семьей",
   },
   {
     id: uuidv4(),
     src: "/images/fears/place.png",
-    desc: "Place icon with a question mark: Embodies the fear of the unknown, especially regarding new places, families, or life changes.",
+    desc: "Страх перед неизвестным, особенно в отношении новых мест, семей или изменений в жизни",
   },
   {
     id: uuidv4(),
     src: "/images/fears/violence.png",
-    desc: "A hand stopping a fist: Indicates the fear of violence.",
+    desc: "Cтрах перед насилием",
   },
   {
     id: uuidv4(),
     src: "/images/fears/dark.png",
-    desc: "A man in a dark room: Means the fear of darkness.",
+    desc: "Страх темноты",
   },
   {
     id: uuidv4(),
     src: "/images/fears/doctor.png",
-    desc: "A doctor: Represents the fear of doctors and the formal procedures associated with orphanages.",
+    desc: "Страх перед врачами и формальными процедурами, связанными с детскими домами и домами сирот",
   },
   {
     id: uuidv4(),
     src: "/images/fears/mask.png",
-    desc: "Two-faced person: An example of the fear of betrayal and lies.",
+    desc: "Страха предательства и лжи",
   },
   {
     id: uuidv4(),
     src: "/images/fears/sad.png",
-    desc: "A girl in rainy weather: Means the fear of loneliness and not having a home.",
+    desc: "Страх одиночества и отсутствия дома",
   },
   {
     id: uuidv4(),
     src: "/images/fears/unknown.png",
-    desc: "A green face with a question mark: Illustrates the fear of unknown people or families.",
+    desc: "Страх перед неизвестными людьми или семьями",
   },
   {
     id: uuidv4(),
     src: "/images/fears/orphan-1.png",
-    desc: "Broken heart and a house: Symbolizes the fear of losing a home and family.",
+    desc: "Страх потери дома и семьи",
   },
 ];
