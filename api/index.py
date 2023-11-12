0b77a983ad5ae327ad051c7fdb2428bcd4e9fa53
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-11-12 14:22:50 +0100

    feat: authentication system added

--- api/index.py
+++ api/index.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 from api.models import Base
 from api.database import engine
-from api.routers import scores
+from api.routers import scores, auth
 
 app = FastAPI()
 
 Base.metadata.create_all(engine)
 
 app.include_router(scores.router)
+app.include_router(auth.router)
